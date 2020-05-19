15f17de2d9c0239b1aef5336185da90f771b0c4a
Håkan Nylén <hakan@dun.se>
2020-05-19 12:52:49 +0200

    require unique address for domain

--- src/operations/save_domain.cr
+++ src/operations/save_domain.cr
@@ -3,4 +3,10 @@ class SaveDomain < Domain::SaveOperation
   # https://luckyframework.org/guides/database/validating-saving#perma-permitting-columns
   #
   permit_columns address, time_zone
+
+  before_save do
+    validate_required address
+    validate_required time_zone
+    validate_uniqueness_of address
+  end
 end
