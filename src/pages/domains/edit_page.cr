8ae77414253cd959d30077789d86d2dc235c1974
Håkan Nylén <hakan@dun.se>
2020-05-20 18:37:23 +0200

    fixed edit page for domain to work
    with timezones

--- src/pages/domains/edit_page.cr
+++ src/pages/domains/edit_page.cr
@@ -12,11 +12,14 @@ class Domains::EditPage < MainLayout
   def render_domain_form(op)
     form_for Domains::Update.with(@domain.id) do
       # Edit fields in src/components/domains/form_fields.cr
-      mount Shared::Field.new(operation.address, "Domain"), &.text_input(autofocus: "true")
-      mount Shared::Field.new(operation.time_zone, "Timezone")
+      mount Shared::Field.new(operation.address, "Domain"), &.text_input(autofocus: "true", append_class: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2")
+      label_for(operation.time_zone, class: "custom-label")
+      select_input(operation.time_zone, class: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2") do
+        options_for_select(operation.time_zone, TIMEZONES.map {|t| {t, t} })
+      end
 
 
-      submit "Update", data_disable_with: "Updating..."
+      submit "Update", data_disable_with: "Updating...", class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
     end
   end
 end
