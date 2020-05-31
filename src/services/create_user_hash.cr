4595789eb4cff5c95019cf29e9cb6cc795473527
Håkan Nylén <hakan@dun.se>
2020-05-31 07:31:28 +0200

    added the secret

--- src/services/create_user_hash.cr
+++ src/services/create_user_hash.cr
@@ -1,7 +1,7 @@
 require "crypto/bcrypt"
 class UserHash
   def self.create(address, ip_address, browser, browser_version) : String
-    OpenSSL::HMAC.hexdigest(OpenSSL::Algorithm::SHA256, "kindmetrics", [address, ip_address, browser, browser_version].join("/"))
+    OpenSSL::HMAC.hexdigest(OpenSSL::Algorithm::SHA256, Lucky::Server.settings.secret_key_base, [address, ip_address, browser, browser_version].join("/"))
   end
 
   def self.get_browser(user_agent)
