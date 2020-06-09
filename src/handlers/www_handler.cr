edabdf68014fa6de8a91b5d32d3a6484579c0a1f
Håkan Nylén <hakan@dun.se>
2020-06-09 19:27:20 +0200

    if someone goes directly to the ip
    redirect to the url - This needs to be fixed later for people running this themselves.

--- src/handlers/www_handler.cr
+++ src/handlers/www_handler.cr
@@ -2,7 +2,10 @@ class WWWHandler
   include HTTP::Handler
 
   def call(context)
-    if context.request.host.not_nil!.starts_with?("www.")
+    if !context.request.host.not_nil!.ends_with?("kindmetrics.io") && Lucky::Env.production?
+      context.response.status_code = 301
+      context.response.headers["Location"] = "https://kindmetrics.io"
+    elsif context.request.host.not_nil!.starts_with?("www.")
       context.response.status_code = 301
       context.response.headers["Location"] = "https://kindmetrics.io#{context.request.path}"
     else
