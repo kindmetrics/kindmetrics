f0e2c006b3114a3f073f205e96708205f36d0ea8
Håkan Nylén <hakan@dun.se>
2020-06-15 16:52:08 +0200

    made a better check on addresses on domains

--- src/operations/mixins/fix_address.cr
+++ src/operations/mixins/fix_address.cr
@@ -1,6 +1,7 @@
 module FixAddress
   macro included
     before_save fix_address
+    before_save validate_address
   end
 
   private def fix_address
@@ -15,5 +16,11 @@ module FixAddress
                     url.host.not_nil!.sub(/^www./i, "")
                   end
     address.value = new_address
+  end
+
+  private def validate_address
+    unless address.value =~ /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}$/
+      address.add_error "is not a valid hostname - should be like 'test.com'"
+    end
   end
 end
