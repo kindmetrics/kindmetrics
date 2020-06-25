d469ffabdffd8bfc031487f5529cab3a5a66be8d
Håkan Nylén <hakan@dun.se>
2020-06-25 14:51:27 +0200

    not saving screen_width so dont need it in db

--- src/models/session.cr
+++ src/models/session.cr
@@ -4,7 +4,6 @@ class Session < BaseModel
     column referrer : String?
     column url : String?
     column referrer_source : String?
-    column screen_width : String?
     column path : String?
     column device : String?
     column operative_system : String?
