020926695b8160c08abeb4dc053f66a39e3bb5a8
Håkan Nylén <hakan@dun.se>
2020-07-01 20:33:24 +0200

    format fix

--- src/services/int_converter.cr
+++ src/services/int_converter.cr
@@ -2,6 +2,7 @@ module JSON::IntConverter(Converter)
   def self.from_json(value : JSON::PullParser) : Int64
     value.read_string.to_i64
   end
+
   def self.to_json(value : Int64, builder : JSON::Builder)
     value.to_json(builder)
   end
