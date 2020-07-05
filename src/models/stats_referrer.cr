8f7a02f09100ce19f0f7a26a06bc07bb41048aff
Håkan Nylén <hakan@dun.se>
2020-07-05 09:19:10 +0200

    fixed referrer medium on dashboard
    also made it so it is actually per unique user

--- src/models/stats_referrer.cr
+++ src/models/stats_referrer.cr
@@ -3,6 +3,7 @@ class StatsReferrer
 
   property referrer_source : String?
   property referrer_domain : String?
+  property referrer_medium : String?
   property count : Int64
   property percentage : Float32?
 end
