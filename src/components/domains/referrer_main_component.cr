233875817452231606b0c6cdb574abc522c5ad67
Håkan Nylén <hakan@dun.se>
2020-07-07 08:12:51 +0200

    using truncate to clean long texts

--- src/components/domains/referrer_main_component.cr
+++ src/components/domains/referrer_main_component.cr
@@ -11,7 +11,7 @@ class ReferrerMainComponent < BaseComponent
         if !event.referrer_domain.nil? && event.referrer_domain.not_nil! == current_domain.address
           text "(direct)"
         else
-          a class: "block px-2 hover:underline", href: current_user.nil? ? Share::Referrer::Show.with(current_domain.hashid, event.referrer_source.to_s, period.to_s).url : Domains::Referrer::Show.with(current_domain.hashid, event.referrer_source.to_s, period.to_s).url do
+          a class: "block px-2 hover:underline truncate", href: current_user.nil? ? Share::Referrer::Show.with(current_domain.hashid, event.referrer_source.to_s, period.to_s).url : Domains::Referrer::Show.with(current_domain.hashid, event.referrer_source.to_s, period.to_s).url do
             img src: "https://api.faviconkit.com/#{event.referrer_domain}/16", class: "inline align-middle mr-2 h-4 w-4 -mt-px"
             text (event.referrer_source || event.referrer_domain).to_s
           end
