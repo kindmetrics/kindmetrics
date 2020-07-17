ebc160455012b8a46f16b6137ad83e4b5ce1fecf
Håkan Nylén <hakan@dun.se>
2020-07-17 12:23:12 +0200

    fixed fallback if not existing

--- src/handlers/www_handler.cr
+++ src/handlers/www_handler.cr
@@ -2,14 +2,14 @@ class WWWHandler
   include HTTP::Handler
 
   def call(context)
-    return call_next(context) if ENV["APP_HOST"]?.nil?
+    return call_next(context) if KindEnv.env("APP_HOST").nil?
 
-    if !context.request.host.not_nil!.ends_with?(KindEnv.env("APP_HOST")) && Lucky::Env.production?
+    if !context.request.host.not_nil!.ends_with?(KindEnv.env("APP_HOST") || "app.kindmetrics.io") && Lucky::Env.production?
       context.response.status_code = 301
-      context.response.headers["Location"] = "https://#{KindEnv.env("APP_HOST")}"
+      context.response.headers["Location"] = "https://#{KindEnv.env("APP_HOST") || "app.kindmetrics.io"}"
     elsif context.request.host.not_nil!.starts_with?("www.")
       context.response.status_code = 301
-      context.response.headers["Location"] = "https://#{KindEnv.env("APP_HOST")}#{context.request.path}"
+      context.response.headers["Location"] = "https://#{KindEnv.env("APP_HOST") || "app.kindmetrics.io"}#{context.request.path}"
     else
       call_next(context)
     end
