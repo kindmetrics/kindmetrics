a67ff9c2e853bcce127bbc8b989417434a8f0500
Håkan Nylén <hakan@dun.se>
2020-07-17 12:04:35 +0200

    fixed app host for www handler

--- src/handlers/www_handler.cr
+++ src/handlers/www_handler.cr
@@ -4,12 +4,12 @@ class WWWHandler
   def call(context)
     return call_next(context) if ENV["APP_HOST"]?.nil?
 
-    if !context.request.host.not_nil!.ends_with?(ENV["APP_HOST"]) && Lucky::Env.production?
+    if !context.request.host.not_nil!.ends_with?(KindEnv.env("APP_HOST")) && Lucky::Env.production?
       context.response.status_code = 301
-      context.response.headers["Location"] = "https://#{ENV["APP_HOST"]}"
+      context.response.headers["Location"] = "https://#{KindEnv.env("APP_HOST")}"
     elsif context.request.host.not_nil!.starts_with?("www.")
       context.response.status_code = 301
-      context.response.headers["Location"] = "https://#{ENV["APP_HOST"]}#{context.request.path}"
+      context.response.headers["Location"] = "https://#{KindEnv.env("APP_HOST")}#{context.request.path}"
     else
       call_next(context)
     end
