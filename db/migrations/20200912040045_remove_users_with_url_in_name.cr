f0685a2c085a0480252a5f973218ab0ae325a34e
Håkan Nylén <hakan@dun.se>
2020-09-12 06:13:07 +0200

    fixed format

--- db/migrations/20200912040045_remove_users_with_url_in_name.cr
+++ db/migrations/20200912040045_remove_users_with_url_in_name.cr
@@ -13,7 +13,7 @@ class RemoveUsersWithUrlInName::V20200912040045 < Avram::Migrator::Migration::V1
     # drop table_for(Thing)
   end
 
-  def no_url_in_name(name : String) :  Bool
+  def no_url_in_name(name : String) : Bool
     matched = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.match(name)
     !matched.nil?
   end
