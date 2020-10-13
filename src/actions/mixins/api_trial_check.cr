61e8c824fed9e92b7680a4e65dad5a8331ae9e28
Håkan Nylén <hakan@dun.se>
2020-10-13 19:22:09 +0200

    check subscription to be correct

--- src/actions/mixins/api_trial_check.cr
+++ src/actions/mixins/api_trial_check.cr
@@ -8,7 +8,7 @@ module ApiTrialCheck
   def check_trial
     return continue if current_user.trial_ends_at > Time.utc
     return continue if current_user.admin?
-    return continue unless subscription_user_check
+    return continue if subscription_user_check
 
     raise LuckyCan::ForbiddenError.new(context)
   end
